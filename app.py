34a8aba84d490c983e0a20d8d889ea4a56283a03
mmsh-web <mm.aler.iran@gmail.com>
2026-07-20 01:10:37 +0330

    Update

--- app.py
+++ app.py
@@ -31,6 +31,12 @@ def index2():
     return render_template('index2.html')
 
 
+@app.route('/shop')
+def index3():
+    print(1)
+    ip = request.remote_addr
+    return render_template('coming soon.html')
+
 if __name__ == '__main__':
     print(f"!- All of your pages start running")
     app.run(host='0.0.0.0', port=4000, debug=True)
